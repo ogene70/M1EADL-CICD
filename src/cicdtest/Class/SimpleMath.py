8913bef6ed73642ac8ee7d0a6e5088ed7fe4cb6f
ogene70 <ogene.joseph@etu.iut-tlse3.fr>
2026-01-19 16:56:27 +0100

    soustraction

--- src/cicdtest/Class/SimpleMath.py
+++ src/cicdtest/Class/SimpleMath.py
@@ -4,3 +4,7 @@ class SimpleMath:
     @staticmethod
     def addition(x:int,y:int)->int:
         return x+y
+    
+    @staticmethod
+    def soustraction(x:int,y:int)->int:
+        return x-y
--- tests/test_simple_math.py
+++ tests/test_simple_math.py
@@ -9,9 +9,18 @@ class TestSimpleMath(unittest.TestCase):
         expected: int = 5
 
         self.assertEqual(SimpleMath.addition(a, b), expected)
+
     def test_addition_false(self) -> None:
         a: int = 2
         b: int = 9
         expected: int = 5
 
         self.assertNotEqual(SimpleMath.addition(a, b), expected)
+
+    def test_soustraction_true(self)-> None:
+        x:int = 6
+        y:int = 3
+
+        expected = 3
+
+        self.assertEqual(SimpleMath.soustraction(6,3),expected)
